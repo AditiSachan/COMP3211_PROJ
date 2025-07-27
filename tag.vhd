278e08d480f49ab5ed22704e2994d86593412895
Jasper-Toole <jaspertoole@gmail.com>
2025-07-28 01:35:08 +1000

    Made tag generic
    Still needs work checking correctness and implementing into the design and making it run-time configurable via opcodes

--- tag.vhd
+++ tag.vhd
@@ -1,181 +1,99 @@
-
---Simplest Specific implementation of tag generation:
---TODO: Ability to change black size via single variable
---all of block partition: Ability to seperate incoming bits into blocks dynamically
---Fix hardcoded values
---May need to change away from using signals in main function and pass sections of the vector instead
+--TODO: Not fully checked yet, needs to be editied to allow for selection of functions
+--via opcodes, and changing of values while running
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity tag is
+  generic(
+    tag_size : integer := 6;
+    bit_size : integer := 31   
+  );
   Port (
-    incoming_bits : in std_logic_vector(15 downto 0);
-    output_tag : out std_logic_vector(3 downto 0)
+    incoming_bits : in std_logic_vector(bit_size downto 0);
+    output_tag : out std_logic_vector(tag_size downto 0)
    );
 end tag;
 
-architecture Behavioral of tag is
-
-component flip is
-  Port (
-      flip_block : in std_logic_vector(3 downto 0);
-      output_block : out std_logic_vector(3 downto 0)
-  );
-end component;
-
-component swap is
-  Port (
-    block_x : in std_logic_vector(3 downto 0);
-    block_y : in std_logic_vector(3 downto 0);
-    p_x : in unsigned(1 downto 0);
-    p_y : in unsigned(1 downto 0);
-    s : in unsigned(3 downto 0);
-    output_x : out std_logic_vector(3 downto 0);
-    output_y : out std_logic_vector(3 downto 0)
-   );
-end component;
-
-component shift is
-  Port (
-    r : in unsigned(3 downto 0);
-    shift_block : in std_logic_vector(3 downto 0);
-    output_block : out std_logic_vector(3 downto 0)
-   );
-end component;
-
---May need to reconfigure into doing operations on the vector instead of signals
---To allow dynamicly resized block size
-signal block1, block2, block3, block4 : std_logic_vector(3 downto 0);
-signal flipped1, flipped2, flipped3, flipped4 : std_logic_vector(3 downto 0);
-signal swapped1, swapped2, swapped3, swapped4 : std_logic_vector(3 downto 0);
-signal shifted1, shifted2, shifted3, shifted4 : std_logic_vector(3 downto 0);
-signal xor_block : std_logic_vector(3 downto 0); 
-
-begin
-
-block1 <= incoming_bits(3 downto 0);
-block2 <= incoming_bits(7 downto 4);
-block3 <= incoming_bits(11 downto 8);
-block4 <= incoming_bits(15 downto 12);
-
-flip1: flip port map ( flip_block => block1, output_block=>flipped1);
-flip2: flip port map ( flip_block => block2, output_block=>flipped2);
-flip3: flip port map ( flip_block => block3, output_block=>flipped3);
-flip4: flip port map ( flip_block => block4, output_block=>flipped4);
-
-swap1: swap port map (
-    block_x => flipped1,
-    block_y => flipped2,
-    p_x => "10",
-    p_y => "00",
-    s => "11",
-    output_x => swapped1,
-    output_y => swapped2
-);
-
-swap2: swap port map (
-    block_x => flipped3,
-    block_y => flipped4,
-    p_x => "01",
-    p_y => "01",
-    s => "01",
-    output_x => swapped3,
-    output_y => swapped4
-);
-
-shift1: shift port map (r=>"0001", shift_block => swapped1, output_block => shifted1);
-shift2: shift port map (r=>"0010", shift_block => swapped2, output_block => shifted2);
-shift3: shift port map (r=>"0100", shift_block => swapped3, output_block => shifted3);
-shift4: shift port map (r=>"1011", shift_block => swapped4, output_block => shifted4);
-
-xor_block <= shifted1 xor shifted2 xor shifted3 xor shifted4;
-
-output_tag <= xor_block;
-
-end Behavioral;
-
----Flip
+--FLIP---------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity flip is
+  generic(tag_size : integer);
   Port (
-    flip_block : in std_logic_vector(3 downto 0);
-    output_block : out std_logic_vector(3 downto 0)
+    flip_block : in std_logic_vector(tag_size downto 0);
+    output_block : out std_logic_vector(tag_size downto 0)
    );
 end flip;
 
-architecture Behavioral of flip is
-
+architecture behavioural of flip is
 begin
+  output_block <= not flip_block;
+end behavioural;
 
-output_block <= not flip_block;
-
-end Behavioral;
-
----Swap
+--SWAP--------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
---NOTE: 's' must be less than or equal to the size of blocks, or you get an error
---because we're reading from block x and y but making changes to tempx and tempy
 entity swap is
+  generic(tag_size : integer);
   Port (
-    block_x : in std_logic_vector(3 downto 0);
-    block_y : in std_logic_vector(3 downto 0);
-    p_x : in unsigned(1 downto 0);
-    p_y : in unsigned(1 downto 0);
-    s : in unsigned(3 downto 0);
-    output_x : out std_logic_vector(3 downto 0);
-    output_y : out std_logic_vector(3 downto 0)
-   );
+    block_x  : in  std_logic_vector(tag_size downto 0);
+    block_y  : in  std_logic_vector(tag_size downto 0);
+    p_x      : in  unsigned(tag_size downto 0);
+    p_y      : in  unsigned(tag_size downto 0);
+    s        : in  unsigned(tag_size downto 0);
+    output_x : out std_logic_vector(tag_size downto 0);
+    output_y : out std_logic_vector(tag_size downto 0)
+  );
 end swap;
 
 architecture Behavioral of swap is
-
-signal result : std_logic_vector(3 downto 0);
-
 begin
 
-  process(block_x, block_y, p_x, p_y, s)
-    variable pos_x : integer;
-    variable pos_y : integer;
-    variable temp_x_bit : std_logic;
-    variable temp_y_bit : std_logic;
-    variable temp_x : std_logic_vector(3 downto 0);
-    variable temp_y : std_logic_vector(3 downto 0);        
-  begin
-    temp_x := block_x;
-    temp_y := block_y;
-    
-    for i in 0 to to_integer(s) - 1 loop
-      pos_x := (to_integer(p_x) + i) mod 4;
-      pos_y := (to_integer(p_y) + i) mod 4;
-        
-      temp_x_bit := block_x(pos_x);
-      temp_y_bit := block_y(pos_y);
-        
-      temp_x(pos_x) := temp_y_bit;
-      temp_y(pos_y) := temp_x_bit;
-    end loop;
-    
-    output_x <= temp_x;
-    output_y <= temp_y;    
-  end process; 
+process(block_x, block_y, p_x, p_y, s)
+  variable temp_x : std_logic_vector(tag_size downto 0);
+  variable temp_y : std_logic_vector(tag_size downto 0);
+  variable pos_x  : integer;
+  variable pos_y  : integer;
+  variable s_int  : integer;
+  variable bit_x  : std_logic;
+  variable bit_y  : std_logic;
+begin
+  temp_x := block_x;
+  temp_y := block_y;
+  s_int := to_integer(s);
+
+  for i in 0 to s_int - 1 loop
+    pos_x := (to_integer(p_x) + i) mod (tag_size + 1);
+    pos_y := (to_integer(p_y) + i) mod (tag_size + 1);
+
+    bit_x := block_x(pos_x);
+    bit_y := block_y(pos_y);
+
+    temp_x(pos_x) := bit_y;
+    temp_y(pos_y) := bit_x;
+  end loop;
+
+  output_x <= temp_x;
+  output_y <= temp_y;
+end process;
+
 end Behavioral;
 
----Shift
+---Shift--------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity shift is
+generic(tag_size : integer);
   Port (
-    r : in unsigned(3 downto 0);
-    shift_block : in std_logic_vector(3 downto 0);
-    output_block : out std_logic_vector(3 downto 0)
+    r : in unsigned(tag_size downto 0);
+    shift_block : in std_logic_vector(tag_size downto 0);
+    output_block : out std_logic_vector(tag_size downto 0)
    );
 end shift;
 
@@ -184,15 +102,91 @@ architecture Behavioral of shift is
 begin
 
   process(r, shift_block)
-    variable temp_block : std_logic_vector(3 downto 0);
+    variable temp_block : std_logic_vector(tag_size downto 0);
   begin
     temp_block := shift_block;
     
     for i in 1 to to_integer(r) loop
-      temp_block := temp_block(2 downto 0) & temp_block(3);
+      temp_block := temp_block(tag_size - 1 downto 0) & temp_block(tag_size);
     end loop;
   
     output_block <= temp_block;
   end process;
+
+end Behavioral;
+
+--MAIN------------------------------------------------
+architecture Behavioral of tag is
+
+--Calcuating numbers needed for making vector length and teg length work
+constant adjustment_size: integer := (tag_size + 1) - ((bit_size + 1) mod (tag_size + 1));
+constant block_count : integer := (tag_size + bit_size + 1) / bit_size + 1;
+constant half_block_count : integer := (block_count - (block_count mod 2))/2;
+constant extended_size : integer := tag_size + adjustment_size;
+
+type block_array is array(0 to block_count - 1) of std_logic_vector(tag_size downto 0);
+
+signal extended_bits : std_logic_vector(extended_size downto 0);
+signal blocks : block_array;
+signal flipped : block_array;
+signal swapped : block_array;
+signal shifted : block_array;
+signal xor_block : std_logic_vector(tag_size downto 0);
+
+begin
+
+extended_bits <= incoming_bits & std_logic_vector(to_unsigned(0, adjustment_size))
+  when adjustment_size > 0 else
+  incoming_bits;
+
+gen_blocks: for i in 0 to block_count - 1 generate
+  blocks(i) <= extended_bits((i+1)*tag_size + i downto i*tag_size + i);
+end generate;
+
+gen_flip: for i in 0 to block_count - 1 generate
+  flip_instance: entity work.flip(behavioural)
+    generic map(tag_size => tag_size)
+    port map(
+      flip_block => blocks(i),
+      output_block => flipped(i)
+    );
+end generate;
+
+gen_swaps: for i in 0 to half_block_count - 1 generate
+  swap_instance: entity work.swap(Behavioral)
+    generic map(tag_size => tag_size)
+    port map (
+      block_x  => flipped(2*i),
+      block_y  => flipped(2*i + 1),
+      p_x      => to_unsigned(0, tag_size + 1),
+      p_y      => to_unsigned(0, tag_size + 1),
+      s        => to_unsigned(tag_size + 1, tag_size + 1),
+      output_x => swapped(2*i),
+      output_y => swapped(2*i + 1)
+    );
+end generate;
+
+gen_shift: for i in 0 to block_count - 1 generate
+  shift_instance: entity work.shift(Behavioral)
+  generic map(tag_size => tag_size)
+  port map (
+    r => to_unsigned(0, tag_size + 1),
+    shift_block => swapped(i),
+    output_block => shifted(i)
+  );
+end generate;
+
+--Xor
+process(shifted)
+  variable temp : std_logic_vector(tag_size downto 0);
+begin
+  temp := (others => '0');
+  for i in 0 to block_count - 1 loop
+    temp := temp xor shifted(i);
+  end loop;
+  xor_block <= temp;
+end process;
+
+output_tag <= xor_block;
 
 end Behavioral;
